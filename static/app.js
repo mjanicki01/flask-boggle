17d5b7ec3df1ea9db46159f7868d69a7a2b5e04e
Magda Janicki <janickimagda@gmail.com>
2021-11-18 13:24:22 -0500

    update jquery script to newest release

--- static/app.js
+++ static/app.js
@@ -1,14 +1,16 @@
+$(document).ready(function() {
 
-const form_input = document.getElementById("id")
+  const form_input = document.getElementById("id")
 
-$('#form').on('submit', function(event) {
-  $.ajax({
-    method: "POST",
-    url: "/",
-    data: { input: form_input }
-  }) 
-    .done(function( input ) {
-      $('#word_display').text(input);
-    });
-  event.preventDefault();
-});
+  $('#form').on('submit', function(event) {
+      $.ajax({
+        method: "POST",
+        url: "/",
+        data: { input: form_input }
+      }) 
+      .done(function( input ) {
+        $('#word_display').text(input);
+      }); 
+    event.preventDefault();
+  });
+})
